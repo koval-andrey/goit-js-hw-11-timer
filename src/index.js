478a6fce34eee005fcb9ace754367073ea183ff0
koval-andrey <kovalandrey1234@gmail.com>
2021-03-03 22:52:38 +0200

    hw-11-timer
    исправление ошибок

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './styles.css';
-import { CountdownTimer } from './script/timer.js';
+import { CountdownTimer} from './script/timer.js';
 
 
 const timerCount = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('may 01, 2021'),
+  targetDate: new Date('May 01, 2021'),
   
 });
 
-timerCount.CountdownTimer();
+timerCount.start();
--- src/script/timer.js
+++ src/script/timer.js
@@ -1,44 +1,47 @@
-export { CountdownTimer};
+export { CountdownTimer };
 
 const daysRef = document.querySelector('span[data-value="days"]');
 const hoursRef = document.querySelector('span[data-value="hours"]');
 const minutesRef = document.querySelector('span[data-value="mins"]');
 const secondsRef = document.querySelector('span[data-value="secs"]');
 
-class CountdownTimer{
-    constructor({ targetDate }){
-  this.targetDate = targetDate; 
-  this.start();
-  this.timerStart();
-};
+class CountdownTimer {
+  constructor({ targetDate }) {
+    this.targetDate = targetDate;
+    this.start();
+    this.timerStart();
+  }
 
-timerStart() {
+  timerStart() {
     const currentDate = Date.now();
     const total = this.targetDate - currentDate;
-    this.updateTimer(this.getTimerParts(total)); 
-}
+    this.updateTimer(this.getTimerParts(total));
+  }
 
-start(){
-    setInterval(() => {this.timerStart()}, 1000);
-}
+  start(){
+    setInterval(() => {
+      this.timerStart();
+    }, 1000);
+  }
 
-pad(value) {
-  return String(value).padStart(2, "0");
-}
-getTimerParts(time) {
-  const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  const hours = this.pad(
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  );
-  const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+  getTimerParts(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-  return {days, hours, mins, secs};
-}
+    return { days, hours, mins, secs };
+  }
 
-updateTimer({days, hours, mins, secs}) {
+  updateTimer({ days, hours, mins, secs }) {
     daysRef.textContent = `${days}`;
     hoursRef.textContent = `${hours}`;
     minutesRef.textContent = `${mins}`;
-    secondsRef.textContent = `${secs}`;  
-}}
+    secondsRef.textContent = `${secs}`;
+  }
+}
